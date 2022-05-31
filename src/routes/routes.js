a16f0dcd4f6a2b4a34a665e3a359c313e35a012e
193228 <81987300+193228@users.noreply.github.com>
2022-05-31 12:31:15 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,4 +15,12 @@ router.post('/createProfile', (req, res) => {
     perfiles.crearPerfiles(req,res)
 })
 
+router.get('/random', async (req, res) => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random")
+    const data = await response.json();
+    return res.json(data);
+    //return res.render('form.ejs');
+})
+
+
 module.exports = router
